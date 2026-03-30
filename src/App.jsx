18311684161e66b7b53bddd2b907e4d18751b273
felixtechlabs-projects <felixtechlabsprojects@gmail.com>
2026-03-30 10:19:56 +0530

    I have changed css

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,6 @@ function App() {
     <Router>
       <div className='w-full h-full'>
         <Header />
-          <button type="button" className="text-white bg-success box-border border border-transparent hover:bg-success-strong focus:ring-4 focus:ring-success-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">Success</button>
       <Routes>
         <Route path='/' element={<RootRedirect />} />
         <Route path='signup' element={<Signup />} />
